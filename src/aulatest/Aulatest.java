ef6440b6c89bd1ba9c81517b09485b416ecaed8a
aluno <aluno@LabX-OptiPlex-7010>
2016-11-21 10:29:48 -0200

    Alteração de código x.

---
+++ src/aulatest/Aulatest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulatest;
+
+/**
+ *
+ * @author aluno
+ */
+public class Aulatest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        boolean novaV= false;
+     
+        int inteiro = 2;
+     
+        if (inteiro <1){
+         novaV= true;
+     }
+        System.out.println("Variavel: "+novaV);   
+        
+        
+    }
+    
+}
